755cb0b2bb5f8b3e32058b77395c8dcd4db8f240
thenaonao <coucou48500@yahoo.fr>
2018-01-02 19:07:46 +0100

    Try to merge1

--- main.cpp
+++ main.cpp
@@ -1,33 +1,76 @@
-#include <iostream>
-#include <string>
-#include <fstream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <iostream>
-#include <WinSock2.h>
-#pragma comment(lib, "Ws2_32.lib")
+#include <windows.h>
 
-int main()
+const char g_szClassName[] = "myWindowClass";
+
+// Step 4: the Window Procedure
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-	WSADATA data;
-	WSAStartup(MAKEWORD(2, 2), &data);
+    switch(msg)
+    {
+        case WM_CLOSE:
+            DestroyWindow(hwnd);
+        break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+        break;
+        default:
+            return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+    return 0;
+}
 
-	SOCKET socket_var = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (socket_var == INVALID_SOCKET)
-	{
-		std::cout << "Erreur creation socket : " << WSAGetLastError() << std::endl;
-		return 0;
-	}
-	sockaddr_in server;
-	server.sin_addr.s_addr = inet_addr("127.0.0.1");
-	server.sin_family = AF_INET;
-	server.sin_port = htons(6666);
-	if (connect(socket_var, (sockaddr*)&server, sizeof(server)) == SOCKET_ERROR)
-	{
-		std::cout << "Erreur connection : " << WSAGetLastError() << std::endl;
-		return 0;
-	}
-	std::cout << "Socket connecte !" << std::endl;
-	closesocket(socket_var);
-	WSACleanup();
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine, int nCmdShow)
+{
+    WNDCLASSEX wc;
+    HWND hwnd;
+    MSG Msg;
+
+    //Step 1: Registering the Window Class
+    wc.cbSize        = sizeof(WNDCLASSEX);
+    wc.style         = 0;
+    wc.lpfnWndProc   = WndProc;
+    wc.cbClsExtra    = 0;
+    wc.cbWndExtra    = 0;
+    wc.hInstance     = hInstance;
+    wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+    wc.lpszMenuName  = NULL;
+    wc.lpszClassName = g_szClassName;
+    wc.hIconSm       = LoadIcon(NULL, IDI_APPLICATION);
+
+    if(!RegisterClassEx(&wc))
+    {
+        MessageBox(NULL, "Window Registration Failed!", "Error!",
+            MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+
+    // Step 2: Creating the Window
+    hwnd = CreateWindowEx(
+        WS_EX_CLIENTEDGE,
+        g_szClassName,
+        "Application",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 720, 480,
+        NULL, NULL, hInstance, NULL);
+
+    if(hwnd == NULL)
+    {
+        MessageBox(NULL, "Window Creation Failed!", "Error!",
+            MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
+
+    // Step 3: The Message Loop
+    while(GetMessage(&Msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&Msg);
+        DispatchMessage(&Msg);
+    }
+    return Msg.wParam;
 }
